27ac11935b625e1a1c0df8271b09c58ccad1e5ce
curlysame <118316442+curlysame@users.noreply.github.com>
2024-09-01 23:48:24 -0300

    Update main_pessoa.py

--- microatividade/main_pessoa.py
+++ microatividade/main_pessoa.py
@@ -1,7 +1,13 @@
 from Pessoa import Pessoa
 
 pessoa1 = Pessoa("João", "2000-01=01", "000.111.222-33", "15975388-1", False)
-pessoa1.alterarStatus(False)
+
+pessoa1.nome = "Ana"
+pessoa1.dataNascimento = "2001-02-02"
+pessoa1.cpf = "123.456.789-12"
+pessoa1.rg = "12345678-2"
+pessoa1.status = True
+
 
 attrs = vars(pessoa1)
 print('Instancia da classe Pessoa: ')
