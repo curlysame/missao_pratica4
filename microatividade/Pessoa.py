1f52a947fa233ebb41dc2b87e4f74bc5233a4bd9
curlysame <118316442+curlysame@users.noreply.github.com>
2024-09-01 23:48:26 -0300

    Update Pessoa.py

--- microatividade/Pessoa.py
+++ microatividade/Pessoa.py
@@ -1,12 +1,55 @@
 class Pessoa:
     def __init__(self, nome, dataNascimento, cpf, rg, status):
-        self.nome = nome
-        self.dataNascimento = dataNascimento
-        self.cpf = cpf
-        self.rg = rg 
-        self.status = status
+        self.__nome = nome
+        self.__dataNascimento = dataNascimento
+        self.__cpf = cpf
+        self.__rg = rg 
+        self.__status = status
+
+    @property
+    def nome(self, nome):
+        return self.__nome
     
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def dataNascimento(self, dataNascimento):
+        return self.__dataNascimento
+    
+    @dataNascimento.setter
+    def  dataNascimento(self, dataNascimento):
+        self.__dataNascimento = dataNascimento
+
+    @property
+    def cpf(self, cpf):
+        return self.__cpf
+    
+    @cpf.setter 
+    def cpf(self,cpf):
+        if len(cpf) != 14:
+            raise ValueError("O CPF deve conter 14 caracteres (no formado000.000.000-00).")
+        
+    @property
+    def rg(self, rg):
+        return self.__rg
 
     
-    def alterarStatus(self, status):
-        self.status = status
+    @rg.setter
+    def rg(self, rg):
+        self.__rg = rg
+            
+        
+    @property 
+    def status(self, status):
+        return self.__status
+
+    status.setter
+    def status(self, status):
+        self.__status = status    
+
+
+
+    
+    
