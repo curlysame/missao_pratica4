599b1f279a5e38498aeb3969bed37c032926074f
curlysame <118316442+curlysame@users.noreply.github.com>
2024-09-02 12:22:16 -0300

    Update Pessoa.py

--- microatividade/Pessoa.py
+++ microatividade/Pessoa.py
@@ -1,9 +1,8 @@
 class Pessoa:
-    def __init__(self, nome, dataNascimento, cpf, rg, status):
+    def __init__(self, nome, numeroConta, dataAbertura, status):
         self.__nome = nome
-        self.__dataNascimento = dataNascimento
-        self.__cpf = cpf
-        self.__rg = rg 
+        self.__numeroConta = numeroConta
+        self.__dataAbertura = dataAbertura
         self.__status = status
 
     @property
@@ -15,30 +14,22 @@ class Pessoa:
         self.__nome = nome
 
     @property
-    def dataNascimento(self, dataNascimento):
-        return self.__dataNascimento
+    def numeroConta(self, numeroConta):
+        return self.__numeroConta
     
-    @dataNascimento.setter
-    def  dataNascimento(self, dataNascimento):
-        self.__dataNascimento = dataNascimento
+    @numeroConta.setter
+    def  numeroConta(self, numeroConta):
+        self.__numeroConta = numeroConta
 
-    @property
-    def cpf(self, cpf):
-        return self.__cpf
-    
-    @cpf.setter 
-    def cpf(self,cpf):
-        if len(cpf) != 14:
-            raise ValueError("O CPF deve conter 14 caracteres (no formado000.000.000-00).")
         
     @property
-    def rg(self, rg):
-        return self.__rg
+    def dataAbertura(self, dataAbertura):
+        return self.__dataAbertura
 
     
-    @rg.setter
-    def rg(self, rg):
-        self.__rg = rg
+    @dataAbertura.setter
+    def dataAbertura(self, dataAbertura):
+        self.__dataAbertura = dataAbertura
             
         
     @property 
